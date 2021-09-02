6b9468e0ce3843278562dce84fbbcfddfbc21f1f
mirakar <mirakar@cisco.com>
2021-09-02 16:01:22 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,8 @@
-"""
-This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
-
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
-"""
+'''
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+'''
 
 from __future__ import print_function
 from meraki_api import MerakiAPI
@@ -36,7 +33,6 @@ def get_ssid_by_name(meraki, org_id, net_id, name):
     raise ValueError("not found: org_id={} net_id={} name={}".format(org_id, net_id, name))
 
 # def meraki.organizations(ORGID).networks().index().json()
-
 
 ####################################################
 
@@ -92,10 +88,10 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Meraki Dev Kit" 
+    speech_output = "Hi from the Meraki Dashboard. How can I help you?" 
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Welcome to the Meraki Dev Kit" 
+    reprompt_text = "Meraki Dashboard is still waiting for your orders." 
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -118,65 +114,12 @@ def get_network_response():
         
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Meraki Kit " \
-                    "More Power. More Choice. More Protection!"
+    speech_output = "The power of Meraki Cloud at your service! " \
+                    "See you later aligator!"
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
-
-
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
 # --------------- Events ------------------
@@ -215,6 +158,16 @@ def on_intent(guest_network, intent_request, session):
         return guest_network.disable()
     if intent_name == "guestnetPASSWORD":
         return guest_network.password()
+    if intent_name == "guestnetSTATUS":
+        return guest_network.checkstatus()
+    if intent_name == "guestnetDEVICES":
+        return guest_network.numdevices()
+    if intent_name == "guestnetTRAFFIC":
+        return guest_network.why_internet_slow()
+    if intent_name == "roadmap":
+        return guest_network.get_roadmap()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
     else:
         raise ValueError("Invalid intent")
 
@@ -228,12 +181,25 @@ def on_session_ended(session_ended_request, session):
     # add cleanup logic here
 
 class GuestNetwork:
-    def __init__(self, meraki, org_id, net_id, ssid):
+    def __init__(self, meraki, meraki_apikey, org_id, net_id, ssid):
         self.meraki             = meraki
+        self.meraki_apikey      = meraki_apikey
         self.org_id             = org_id
         self.net_id             = net_id
         self.ssid               = ssid
 
+    def getdevices(self):
+        
+        url = "https://api.meraki.com/api/v0/networks/" + self.net_id + "/clients"
+        headers = {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                    "X-Cisco-Meraki-API-Key": self.meraki_apikey
+                    }
+        response = requests.request('GET', url, headers=headers)
+        return response.json() 
+        
+        #enable the guest Wi-FI
     def enable(self):
         session_attributes = {}
         card_title = "Guest Network On"
@@ -246,6 +212,7 @@ class GuestNetwork:
         print(speech_output)
         return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
 
+        #disable the guest Wi-Fi
     def disable(self):
         session_attributes = {}
         card_title = "Guest Network Off"
@@ -258,17 +225,92 @@ class GuestNetwork:
         print(speech_output)
         return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
 
+        #share the Guest Wi-Fi password
     def password(self):
         session_attributes = {}
         card_title = "Guest Network Password"
-        speech_output = "The password for {0} - is {1}".format(str(self.ssid['name']),  self.ssid['psk'])
         reprompt_text = ""
+        response =self.meraki.organizations(self.org_id).networks(self.net_id).ssids(self.ssid['number']).index().json()['authMode']
+        if response == "open":
+            speech_output = "There is no password for this Guest Wi-Fi. Just connect, click through the splash page and enjoy!"
+        else:
+            speech_output = "The password for {0} - is {1}".format(str(self.ssid['name']),  self.ssid['psk'])
         should_end_session = True
         return build_response(session_attributes, build_speechlet_response(
             card_title, speech_output, reprompt_text, should_end_session))
 
+        #check if the Guest Wi-Fi is enabled or disabled
+    def checkstatus(self):
+        session_attributes = {}
+        card_title = "Check Status"
+        reprompt_text = ""
+        #devces = getdevices(self)
+        response = self.meraki.organizations(self.org_id).networks(self.net_id).ssids(self.ssid['number']).index().json()['enabled']
+        if response:
+            speech_output = "Guest Wi-Fi is enabled!"
+        else:
+            speech_output = "Guest Wi-Fi is disabled!"
+        print(speech_output)
+        return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
+     
+        #check the number of devices that are connected on the guest Wi-Fi
+    def numdevices(self):
+        session_attributes = {}
+        reprompt_text = None
+        card_title = "Number of devices"
+        
+        num_device = len([device for device in self.getdevices() if device['ssid'] == self.ssid])
+        
+        speech_output = 'The number of connected devices is {0}'.format(num_device)
+        print(speech_output)
+        return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
+    
+        
+    
+    def getnetworktrafficstats(self):
+        #https://developer.cisco.com/meraki/api/#!get-network-traffic
+        url = "https://api.meraki.com/api/v0/networks/" + self.net_id + "/traffic?timespan=86400&deviceType=combined"
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "X-Cisco-Meraki-API-Key": self.meraki_apikey
+                    }
+        response = requests.request('GET', url, headers=headers)
+        return response.json()        
 
+        #check the network traffic
+    def why_internet_slow(self):
+         
+        session_attributes = {}
+        reprompt_text = None
+        card_title = "Why is the conncetion slow?"
+        
+        #86400 seconds = 24 hours 
+        TIMESTAMP = 86400
+        
+        clients = self.getdevices()
+        usage = [client['usage']['sent'] + client['usage']['recv'] for client in clients]
+        max_client = clients[usage.index(max(usage))]
+        bw_use = round((max_client['usage']['sent'] + max_client['usage']['recv']) / 1024)
+    
+        traffic = self.getnetworktrafficstats()
+        applications = [app['application'] for app in traffic]
+        bws = [app['recv'] + app['sent'] for app in traffic]
+        max_bw = max(bws)
+        max_app = applications[bws.index(max_bw)]
+    
+        speech_output = 'That\'s because among the {0} clients connected, the top bandwidth hog is {1}. That device used {2} megabytes of data in the last {3}, {4} percent just on {5}.'.format(len(clients), max_client['description'], bw_use, ('hour' if TIMESTAMP == 3600 else str(round(TIMESTAMP / 60 / 60)) + ' hours'), round((max_bw / 1024) / bw_use * 100), max_app)
+        return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
 
+        # an easter egg for Merakians =)
+    def get_roadmap(self):
+        session_attributes = {}
+        card_title = "Roadmap"
+        reprompt_text = None
+        speech_output = 'The first rule of Meraki roadmaps, ' \
+                        'is we do not talk about Meraki roadmaps.'
+        return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, False))
+        
 # --------------- Main handler ------------------
 
 def lambda_handler(event, context):
@@ -281,7 +323,7 @@ def lambda_handler(event, context):
     org              = get_org_by_name(meraki, MERAKI_ORG_NAME)
     net              = get_network_by_name(meraki, org['id'], MERAKI_NET_NAME)
     ssid             = get_ssid_by_name(meraki, org['id'], net['id'], MERAKI_SSID_NAME)
-    guest_network    = GuestNetwork(meraki, org['id'], net['id'], ssid)
+    guest_network    = GuestNetwork(meraki, MERAKI_API_KEY, org['id'], net['id'], ssid)
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
